831bd94a8f8dc4bf91334966f474b80f433211cd
anh.do <tuananh9h@gmail.com>
2024-07-11 15:24:14 +0700

    REST API with Prisma ORM, Neon Postgres

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('/favicon.ico')
+  serveFavicon(@Res() res) {
+    return res.sendFile(join(__dirname, '..', 'public', 'favicon.ico'));
+  }
 
   @Get()
   getHello(): string {
